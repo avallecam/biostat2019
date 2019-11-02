f9e805e59c205b304005a70e8cb156593dd14459
avallecam <avallecam@gmail.com>
2019-11-02 04:21:30 -0500

    add microarray data via script

--- practica-05.R
+++ practica-05.R
@@ -3,6 +3,29 @@
 
 # practica 05 -------------------------------------------------------------
 
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(broom)
+library(qvalue)
+
+url <- "http://varianceexplained.org/files/Brauer2008_DataSet1.tds"
+
+nutrient_names <- c(G = "Glucose", L = "Leucine", P = "Phosphate",
+                    S = "Sulfate", N = "Ammonia", U = "Uracil")
+
+cleaned_data <- read_delim(url, delim = "\t") %>%
+  separate(NAME, c("name", "BP", "MF", "systematic_name", "number"), sep = "\\|\\|") %>%
+  mutate_each(funs(trimws), name:systematic_name) %>%
+  select(-number, -GID, -YORF, -GWEIGHT) %>%
+  gather(sample, expression, G0.05:U0.3) %>%
+  separate(sample, c("nutrient", "rate"), sep = 1, convert = TRUE) %>%
+  mutate(nutrient = plyr::revalue(nutrient, nutrient_names)) %>%
+  filter(!is.na(expression), systematic_name != "")
+
+write_rds(cleaned_data,"data/microarraydata.rds")
+
 # importar datos ----------------------------------------------------------
 
 cleaned_data <- read_rds("data-raw/microarraydata.rds")
